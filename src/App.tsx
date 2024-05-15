392d63532d137a632f6b0bd097a4e05d513174fb
William Wu <williamwuu3@gmail.com>
2024-05-15 14:33:16 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from "./component/Hero"
 import Service from "./component/Service";
+import About from "./component/About";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <div className="bg-black text-white">
       <Hero />
       <Service restBase={restBase} />
+			<About restBase={restBase} />
     </div>
   )
 }
