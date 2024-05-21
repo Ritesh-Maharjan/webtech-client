f91b3bb4460c6ec40883165eeb653f3ab877b991
William Wu <williamwuu3@gmail.com>
2024-05-21 12:04:50 -0700

    finished our rules section

--- src/component/Accordion.tsx
+++ src/component/Accordion.tsx
@@ -3,6 +3,16 @@ import React, { forwardRef, useEffect, useState } from "react";
 interface PageContent {
   id: number;
   content: { rendered: string };
+  acf: {
+    experience: string;
+    working_with_passion: string;
+    accessibility: string;
+    security: string;
+    communication: string;
+    bespoke: string;
+    work_ethics: string;
+    feedback: string;
+  };
 }
 
 interface AccordionProps {
@@ -10,7 +20,7 @@ interface AccordionProps {
 }
 
 const Accordion = forwardRef<HTMLDivElement, AccordionProps>(({ restBase }, ref) => {
-  const [pageContent, setPageContent] = useState<string>("");
+  const [pageContent, setPageContent] = useState<PageContent | null>(null);
 
   useEffect(() => {
     const fetchPageContent = async () => {
@@ -20,7 +30,7 @@ const Accordion = forwardRef<HTMLDivElement, AccordionProps>(({ restBase }, ref)
           throw new Error("Failed to fetch page content");
         }
         const data: PageContent = await response.json();
-        setPageContent(data.content.rendered);
+        setPageContent(data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -29,17 +39,52 @@ const Accordion = forwardRef<HTMLDivElement, AccordionProps>(({ restBase }, ref)
     fetchPageContent();
   }, [restBase]);
 
-	return (
+  return (
     <div id="accordion" ref={ref} className="max-width h-fit p-4 lg:h-screen">
-      <h2 className="text-4xl text-center">Our Rules</h2>
-      {/* <div className="p-4 max-w-lg" dangerouslySetInnerHTML={{ __html: pageContent }} /> */}
-
-      <div className="bg-gray-700 p-4 rounded-md">
-        {/* Accordion Us Content */}
-        <div className="">
-          <div dangerouslySetInnerHTML={{ __html: pageContent }} />
-        </div>
-      </div>
+      <h2 className="text-4xl text-center bg-cyan-500/50">Our Rules</h2>
+      {pageContent && (
+        <>
+          <p dangerouslySetInnerHTML={{ __html: pageContent.content.rendered }} />
+          <div className=" p-4 rounded-md">
+            <div className="">
+              <ul className="flex flex-wrap justify-between">
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">EXPERIENCE</h3>
+                  {pageContent.acf.experience}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">WORK WITH PASSION</h3>
+                  {pageContent.acf.working_with_passion}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">ACCESSIBILITY</h3>
+                  {pageContent.acf.accessibility}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">SECURITY</h3>
+                  {pageContent.acf.security}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">COMMUNICATION</h3>
+                  {pageContent.acf.communication}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">BESPOKE</h3>
+                  {pageContent.acf.bespoke}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">WORK ETHICS</h3>
+                  {pageContent.acf.work_ethics}
+                </li>
+                <li className="w-full sm:w-1/2 md:w-1/3 lg:w-1/3 p-4">
+                  <h3 className="text-2xl font-bold">FEEDBACK</h3>
+                  {pageContent.acf.feedback}
+                </li>
+              </ul>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 });
